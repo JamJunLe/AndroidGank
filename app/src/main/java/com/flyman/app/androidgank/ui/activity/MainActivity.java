0d1b1e39a8846a7a8438d92ec0d54a04b763be78
flymanmolo <flymanmolo@gmail.com>
2017-10-26 23:26:18 +0800

    //

--- app/src/main/java/com/flyman/app/androidgank/ui/activity/MainActivity.java
+++ app/src/main/java/com/flyman/app/androidgank/ui/activity/MainActivity.java
@@ -99,6 +99,7 @@ public class MainActivity extends SubscriberActivity implements NavigationView.O
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        //
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
